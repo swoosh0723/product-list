2df173dcad845ae998ed23b2be73616e3c60f7a7
Kang Hyeonseok <delantero0723@naver.com>
2022-03-15 23:57:47 +0900

    [STUDY-20220315] 상품 api

--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import store from './store/store'
 
-import { Provider } from 'react-redux'
-import { combineReducers, createStore } from 'redux';
+// import { Provider } from 'react-redux'
+// import store from './store/store'
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
