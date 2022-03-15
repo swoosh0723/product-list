6d515faa7fce9bdb2233d32ecaf410239f5d0c72
Kang Hyeonseok <delantero0723@naver.com>
2022-03-16 02:04:04 +0900

    [STUDY-20220316] 가격 콤마, sold out, 단독 라벨, 이미지 404 일때

--- src/components/GoodsList.js
+++ src/components/GoodsList.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import axios from 'axios';
 import styled from 'styled-components'
@@ -7,56 +7,31 @@ import Goods from './Goods'
 const GoodsListBox = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
+  gap: 12px 0;
 `
 
 function GoodsList(props) {
-  const [goodsData, setGoodsData] = useState(
-    [
-      {
-        "id": 6,
-        "image": "//image.msscdn.net/images/goods_img/20190510/1041436/1041436_2_320.jpg",
-        "brand": "무신사 스탠다드",
-        "name": "무신사 스탠다드 양말",
-        "content": "Born in France",
-        "price": 120000,
-        "rate": "30%",
-        "stock": 1
-      },
+  const [goodsData, setGoodsData] = useState([]);
 
-      {
-        "id": 7,
-        "image": "//image.msscdn.net/images/goods_img/20210624/2008928/2008928_6_320.jpg",
-        "brand": "뉴발란스",
-        "name": "슬리퍼",
-        "content": "Born in Seoul",
-        "price": 110000,
-        "rate": "20%",
-        "stock": 2
-      },
-    ]
-  );
+  useEffect(() => {
+    axios.get('https://static.msscdn.net/musinsaUI/homework/data/goods0.json')
+      .then((result) => {
 
-  console.log(goodsData)
+        const goodsAPI = result.data.data.list
 
-  axios.get('https://static.msscdn.net/musinsaUI/homework/data/goods0.json')
-    .then((result) => {
+        const goodsTest = [
+          ...goodsAPI
+        ]
 
-      const goodsAPI = result.data.data.list
-
-      const goodsTest = [
-        ...goodsData,
-        ...goodsAPI
-      ]
-
-      setGoodsData(goodsTest)
-    })
-    .catch(() => {
-      console.log('실패임')
-    });
+        setGoodsData(goodsTest)
+      })
+      .catch(() => {
+        console.log('실패임')
+      });
+  }, []);
 
   return (
     <GoodsListBox>
-
       {
         goodsData.map((item, i) => {
           return (
@@ -65,7 +40,6 @@ function GoodsList(props) {
               goodsData={item}
             >
             </Goods>
-
           )
         })
       }
